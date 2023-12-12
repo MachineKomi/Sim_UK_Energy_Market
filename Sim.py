3f833557a4c21a12306a17e9f1df07ff2e4f3294
MachineKomi <136615872+MachineKomi@users.noreply.github.com>
2023-12-12 16:36:11 +0000

    Sim0.0.1.py

--- Sim.py
+++ Sim.py
@@ -13,11 +13,21 @@ baseline_data = {
     },
     "weather_conditions": {
         "average_temperature": 10,
-        "wind_speed": 15
+        "wind_speed": 15,
+        "solar_irradiance": 800  # Added solar irradiance in baseline
     }
 }
 
+# Utility function for validation
+def validate_input(data, required_keys):
+    if not all(key in data for key in required_keys):
+        raise ValueError(f"Missing one or more required keys: {required_keys}")
+
+# Enhanced electricity generation function
 def calculate_electricity_generation_advanced(economic_conditions, weather_data, energy_market_conditions):
+    validate_input(weather_data, ["wind_speed", "solar_irradiance"])
+    validate_input(energy_market_conditions, ["electricity_prices"])
+
     baseline_generation = {
         "gas": 30000,
         "coal": 10000,
@@ -26,23 +36,28 @@ def calculate_electricity_generation_advanced(economic_conditions, weather_data,
         "nuclear": 9000
     }
     wind_speed_factor = weather_data["wind_speed"] / 15
-    baseline_generation["wind"] *= wind_speed_factor
     solar_irradiance_factor = weather_data["solar_irradiance"] / 1000
+    price_elasticity = 0.1  # Adjust as per realistic models
+
+    # Adjust generation based on weather and price
+    baseline_generation["wind"] *= wind_speed_factor
     baseline_generation["solar"] *= solar_irradiance_factor
-    price_factor = energy_market_conditions["electricity_prices"] / 100
-    baseline_generation["gas"] *= (1 / price_factor)
-    baseline_generation["coal"] *= (1 / price_factor)
+    baseline_generation["gas"] *= 1 + price_elasticity * (100 - energy_market_conditions["electricity_prices"])
+    baseline_generation["coal"] *= 1 + price_elasticity * (100 - energy_market_conditions["electricity_prices"])
+
     total_generation = sum(baseline_generation.values())
     return {
         "generation_by_source": baseline_generation,
         "total_generation": total_generation
     }
 
+# Updated transmission network simulation
 def simulate_transmission_network(electricity_generated, weather_conditions):
+    validate_input(weather_conditions, ["average_temperature"])
+
     baseline_capacity = 50000
-    baseline_efficiency = 95
-    if weather_conditions["average_temperature"] > 25:
-        baseline_efficiency -= 3
+    baseline_efficiency = 95 - 0.2 * max(weather_conditions["average_temperature"] - 25, 0)  # Gradual efficiency reduction
+
     actual_transmission = min(electricity_generated, baseline_capacity) * (baseline_efficiency / 100)
     return {
         "capacity": baseline_capacity,
@@ -50,12 +65,20 @@ def simulate_transmission_network(electricity_generated, weather_conditions):
         "actual_transmission": actual_transmission
     }
 
+# Dynamic gas network simulation
 def simulate_gas_network(economic_conditions, energy_market_conditions):
+    validate_input(economic_conditions, ["inflation_rate", "gdp_growth_rate"])
+    validate_input(energy_market_conditions, ["gas_prices"])
+
     baseline_production = 500
+    price_elasticity = 0.05  # Adjust as per realistic models
     price_factor = energy_market_conditions["gas_prices"] / 75
-    adjusted_production = baseline_production * (1 / price_factor)
-    gas_import = 200
-    gas_export = 100
+    adjusted_production = baseline_production * (1 + price_elasticity * (75 - energy_market_conditions["gas_prices"]))
+
+    # Dynamic gas import/export based on economic conditions
+    gas_import = 200 * (1 + economic_conditions["gdp_growth_rate"] / 100)
+    gas_export = 100 * (1 + economic_conditions["inflation_rate"] / 100)
+
     net_gas_availability = adjusted_production + gas_import - gas_export
     return {
         "production": adjusted_production,
@@ -64,13 +87,21 @@ def simulate_gas_network(economic_conditions, energy_market_conditions):
         "net_availability": net_gas_availability
     }
 
+# Updated GSP group simulation
 def simulate_gsp_group_import_export(gsp_groups, economic_conditions, energy_market_conditions):
     gsp_results = {}
     for gsp in gsp_groups:
-        electricity_import = gsp["electricity_import"] * (energy_market_conditions["electricity_prices"] / 100)
-        electricity_export = gsp["electricity_export"] * (energy_market_conditions["electricity_prices"] / 100)
-        gas_import = gsp["gas_import"] * (energy_market_conditions["gas_prices"] / 75)
-        gas_export = gsp["gas_export"] * (energy_market_conditions["gas_prices"] / 75)
+        validate_input(gsp, ["electricity_import", "electricity_export", "gas_import", "gas_export"])
+
+        # Dynamic pricing impact
+        electricity_import_price_factor = energy_market_conditions["electricity_prices"] / 100
+        gas_price_factor = energy_market_conditions["gas_prices"] / 75
+
+        electricity_import = gsp["electricity_import"] * electricity_import_price_factor
+        electricity_export = gsp["electricity_export"] * electricity_import_price_factor
+        gas_import = gsp["gas_import"] * gas_price_factor
+        gas_export = gsp["gas_export"] * gas_price_factor
+
         net_electricity_import_export = electricity_import - electricity_export
         net_gas_import_export = gas_import - gas_export
         gsp_results[gsp["name"]] = {
